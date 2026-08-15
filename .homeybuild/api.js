1c04e987aaa245e176077c92e06680235fe33bc9
Davy Bert <87725911+DavyBert@users.noreply.github.com>
2026-08-16 00:09:23 +0200

    Ignore Homey build files

--- .homeybuild/api.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  async listSources({ homey }) { return homey.app.listSources(); },
-  async getSelection({ homey }) { return homey.app.getSelection(); },
-  async saveSelection({ homey, body }) { return homey.app.saveSelection((body && body.selection) || []); },
-  async getConfig({ homey }) { return homey.app.getConfig(); },
-  async saveConfig({ homey, body }) { return homey.app.saveConfig(body || {}); },
-  async testConnection({ homey }) { return homey.app.testConnection(); }
-};
--- .homeybuild/widgets/dashboard/api.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-module.exports = {
-  async getDashboard({ homey }) { return homey.app.getDashboard(); }
-};
